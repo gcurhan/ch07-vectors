897f07f601968cc3f5aaaf33984fec6b7bf58184
Gabe Curhan <gcurhan@uw.edu>
2018-10-10 15:59:36 -0700

    first try

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,4 +1,4 @@
-# Exercise 1: Creating and Indexing Vectors
+"# Exercise 1: Creating and Indexing Vectors
 
 # Create a vector `first.ten` that has the values 10 through 20 in it (using the : operator)
 
